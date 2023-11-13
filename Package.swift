7e2b59966930cdef3f2d83ed331722b2bde399a2
Batuhan Göbekli <batuhangobekli@gmail.com>
2023-11-13 15:03:51 +0300

    update package

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .package(url: "https://github.com/TakeScoop/SwiftyRSA.git", from: "1.0.0"),
         .package(url: "https://github.com/Tap-Payments/SharedDataModels-iOS.git", from: "0.0.1"),
         .package(url: "https://github.com/Tap-Payments/TapCardScannerWebWrapper-iOS.git", exact: "0.0.6"),
-        .package(url: "https://github.com/batuhangobekli/TapFontKit-iOS", from: "0.0.1")
+        .package(url: "https://github.com/batuhangobekli/TapFontKit-iOS.git", from: "0.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
